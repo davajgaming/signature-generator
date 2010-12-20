348bbe3ea3332764093e9849b54608dca67fffe9
Sam Thompson <sam@websyntax.net>
2010-12-19 23:13:32 -0800

    Overhauled stuff

---
+++ signature/plugins/fungineer/plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+*
+* @package Fungineer Plugin
+* @copyright (c) 2010 Sam Thompson
+* @license http://www.opensource.org/licenses/mit-license.php The MIT License
+*
+*/
+
+if(!defined('SIGGEN_ROOT'))
+	exit;
