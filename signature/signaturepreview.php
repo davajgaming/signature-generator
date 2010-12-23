38f0fc529e841dfa2a5226a1e8f00c6686f1d2af
Sam Thompson <sam@websyntax.net>
2010-12-23 01:54:10 -0800

    Started (and almosted completed) work on our AJAX preview handler

--- signature/signaturepreview.php
+++ signature/signaturepreview.php
@@ -8,4 +8,12 @@
  */
 
 define('SIGGEN_ROOT', dirname(__FILE__) . '/');
+include SIGGEN_ROOT . 'signature_factory.php';
 
+$theme = preg_replace("#[^a-z0-9]#i", '', $_REQUEST['theme']);
+$signature = new Signature_Factory();
+
+$signature->loadTheme($theme)
+	->loadUserData(array(0, $data))
+	->buildSignature()
+	->outputStream();
