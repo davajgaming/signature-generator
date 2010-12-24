cabeff07c9ce3a0340f5843dedf056e2ceb675a1
Sam Thompson <sam@websyntax.net>
2010-12-23 23:43:12 -0800

    Field builder pretty much complete

--- signature/src/field_builder.php
+++ signature/src/field_builder.php
@@ -13,6 +13,7 @@ if(!defined('SIGGEN_ROOT'))
 class Field_Builder
 {
 	private $theme = array();
+	private $dbRow = array();
 
 	public function __costruct()
 	{
@@ -21,9 +22,94 @@ class Field_Builder
 	public function loadTheme($theme)
 	{
 		$this->theme = $theme;
+		return $this;
 	}
 
 	public function loadDefaults($data)
 	{
+		$this->dbRow = $data;
+		return $this;
+	}
+
+	/**
+	 * Builds everything for the template
+	 *
+	 * @return array
+	 */
+	public function factory()
+	{
+		// our container for the value to be returned
+		$html = array();
+
+		// The loop... of somewhat epic proportions
+		foreach($this->theme['fields'] as $id => $field)
+		{
+			$max = $min = false;
+
+			if(strpos(':', $field['input']))
+			{
+				@list($type, $max, $min) = explode(':', $field['input']);
+			}
+			else
+			{
+				$type = $field['input'];
+			}
+
+			// switch out our fields
+			switch($type)
+			{
+				case 'text':
+				case 'int':
+					$input = sprintf(TPL_INPUT_TEXT, $id, $this->dbRow[$id]);
+				break;
+
+				case 'select':
+					$multiple = ($max || $min)  ? ' multiple="multiple"' : '';
+					$options = '';
+
+					foreach($field['data'] as $val => $text)
+					{
+						$sel = ($this->dbRow[$id] == $val) ? ' selected="selected' : '';
+						$options .= sprintf(TPL_INPUT_OPTION, $val, $sel, $text);
+					}
+
+					$input = sprintf(TPL_INPUT_SELECT, $id, $multiple, $options);
+				break;
+
+				case 'radio':
+					$input = '';
+					foreach($field['data'] as $val => $text)
+					{
+						$sel = ($this->dbRow[$id] == $val) ? ' checked="checked' : '';
+						$input .= sprintf(TPL_INPUT_RADIO, $id, $val, $sel, $text);
+					}
+
+				break;
+
+				case 'color':
+					// @todo
+				break;
+
+				case 'checkbox':
+					$sel = ($this->dbRow[$id] == $val) ? ' checked="checked' : '';
+					$input = sprintf(TPL_INPUT_CHECKBOX, $id, $sel, $this->dbRow[$id]);
+				break;
+
+				case 'system':
+				case 'level':
+					$input = sprintf(TPL_INPUT_HIDDEN, $id, $this->dbRow[$id]);
+				break;
+
+				default:
+					continue;
+				break;
+			}
+
+			$html[] = array(
+				'title'	=> empty($field['title']) ? '' : $field['title'],
+				'desc'	=> empty($field['desc']) ? '' : $field['desc'],
+				'input'	=> $input,
+			);
+		}
 	}
 }
